52a6b20aad47f95a10de5e53bea186fb71472603
Nathan Bagge <nathan@MAC-3B30BB.station>
2026-09-14 12:12:37 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -20,6 +20,15 @@ const friends = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    }
+}
+
 const colleague1 = {
   name: "Ralph Graham",
   department: "Engineering",
